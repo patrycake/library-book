a56f8ecb93fbbd695fcc6af3debfc8a954479e0b
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-12 16:03:25 -0500

    basic html form + addBookToLibrary pseudocode

--- library.js
+++ library.js
@@ -1,9 +1,17 @@
-function Book (title, author, pages, read){
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info =  function () {
-    return `${title}, ${author}, ${pages} pages, ${read ? "Already read" : "Not read yet"}`;
+    this.info = function () {
+        return `${title}, ${author}, ${pages} pages, ${read ? "Already read" : "Not read yet"}`;
     }
+}
+
+function addBookToLibrary() {
+    //users input
+    userBook = new Book(userTitle, userAuthor, userPages, userRead);
+    myLibrary.push(userBook)
 }
