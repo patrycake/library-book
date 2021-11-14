9700a75ffc9720f97567b268918955d61aaa4815
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-13 21:25:20 -0500

    readStatusButton changes status oesnt update gui

--- library.js
+++ library.js
@@ -1,14 +1,23 @@
 let myLibrary = [];
+
 var addBookButton = document.getElementById("new-book");
 var bookForm = document.getElementById("book-form");
 var closeFrom = document.getElementById("close-form");
-var libraryDisplayDOM = document.getElementById("library-display")
+var libraryDisplayDOM = document.getElementById("library-display");
+var readStatusButtons = document.getElementsByClassName("read-status-button");
+var removeBookButtons = document.getElementsByClassName("remove-button")
 
-addBookButton.addEventListener("click", function () {
+Array.from(readStatusButtons).forEach(butt => {
+    butt.addEventListener("click", () => {
+        readStatus = myLibrary[butt.id.length].read;
+        readStatus ? myLibrary[butt.id.length].read = false : myLibrary[butt.id.length].read = true;
+    })
+})
+addBookButton.addEventListener("click", () => {
     bookForm.style.display = "block";
 })
 
-closeFrom.addEventListener("click", function () {
+closeFrom.addEventListener("click", () => {
     bookForm.style.display = "none";
 })
 
@@ -20,7 +29,8 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        return `${title}, ${author}, ${pages} pages, ${read ? "Already read" : "Not read yet"}`;
+        return `${title}, ${author}, ${pages} pages, 
+        ${read ? "Already read" : "Not read yet"}`;
     }
 }
 
@@ -40,16 +50,26 @@ function addBookToLibrary(event) {
 }
 
 function displayLibrary(userBook) {
-    bookDiv = document.createElement("div");
-    bookTitle = document.createElement("h2");
-    bookInfo = document.createElement("p");
+    let bookDiv = document.createElement("div");
+    let bookTitle = document.createElement("h2");
+    let bookInfo = document.createElement("p");
+    let removeButton = document.createElement("button");
+    let readButton = document.createElement("button")
 
     bookDiv.classList.add("card")
     bookTitle.innerText = userBook.title;
     bookInfo.innerText = `Author: ${userBook.author}, 
         Page Number: ${userBook.pages}, 
         Read: ${userBook.read}`;
+    readButton.innerText = `${userBook.read ? "Not Read" : "Read"}`
+    removeButton.innerText = "Remove Book";
+    readButton.classList.add("read-status-button")
+    removeButton.classList.add("remove-button")
+    readButton.id = `read-${myLibrary.length}`;
+
     bookDiv.appendChild(bookTitle);
     bookDiv.appendChild(bookInfo);
+    bookDiv.appendChild(readButton);
+    bookDiv.appendChild(removeButton)
     libraryDisplayDOM.appendChild(bookDiv)
 }
